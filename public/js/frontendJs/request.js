f64eb60332510398a2b02dc3a225a171b576e7b9
Ajay Korat <ajaykorat16@gmail.com>
2024-07-17 16:56:01 +0530

    few changes to the website.

--- public/js/frontendJs/request.js
+++ public/js/frontendJs/request.js
@@ -1,19 +1,27 @@
 $(document).ready(function() {
 
-    $('.get-quote-button').on('click', function(event) {
+    // Smooth scroll to the request form when the get-quote button is clicked
+    $('.get-quote-button').off('click').on('click', function(event) {
         event.preventDefault();
+        console.log('Get quote button clicked');
         const requestForm = $('#requestForm');
 
-        $('html, body').animate({
-            scrollTop: requestForm.offset().top
-        }, 1000);
-
-        requestForm.show();
+        if (requestForm.length) {
+            $('body').animate({
+                scrollTop: requestForm.offset().top
+            }, 1000, function() {
+                console.log('Scroll animation completed');
+                requestForm.show();
+            });
+        } else {
+            console.error('Request form not found.');
+        }
     });
 
-
-    $('#request-form').on('submit', function(event) {
+    // Handle form submission via AJAX
+    $('#request-form').off('submit').on('submit', function(event) {
         event.preventDefault();
+        console.log('Request form submitted');
         var $form = $(this);
 
         $.ajax({
@@ -21,6 +29,7 @@ $(document).ready(function() {
             url: $form.attr('action'),
             data: $form.serialize(),
             success: function(response) {
+                console.log('AJAX request successful');
                 var responseHtml = $(response);
                 var newFormHtml = responseHtml.find('#request-form').html();
                 var successMessage = responseHtml.find('.flash-messages').html();
@@ -37,26 +46,33 @@ $(document).ready(function() {
                 }
             },
             error: function(response) {
+                console.error('AJAX request failed');
                 alert('An error occurred. Please try again.');
             }
         });
     });
 
+    // Scroll to the success message
     function scrollToSuccessMessage() {
         var successMessage = $('.flash-messages');
         if (successMessage.length > 0) {
             $('html, body').animate({
                 scrollTop: successMessage.offset().top
-            }, 'slow');
+            }, 'slow', function() {
+                console.log('Scroll to success message completed');
+            });
         }
     }
 
+    // Scroll back to the form in case of errors
     function scrollToForm() {
         var requestForm = $('#requestForm');
         if (requestForm.length > 0) {
             $('html, body').animate({
                 scrollTop: requestForm.offset().top
-            }, 'slow');
+            }, 'slow', function() {
+                console.log('Scroll to form completed');
+            });
         }
     }
 });
