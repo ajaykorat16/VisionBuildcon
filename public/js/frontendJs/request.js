159e91d8931d0b6b5eb74d089d8984e462399658
Ajay Korat <ajaykorat16@gmail.com>
2024-07-23 14:13:30 +0530

    few changes in content

--- public/js/frontendJs/request.js
+++ public/js/frontendJs/request.js
@@ -1,16 +1,13 @@
 $(document).ready(function() {
 
-    // Smooth scroll to the request form when the get-quote button is clicked
-    $('.get-quote-button').off('click').on('click', function(event) {
+    $('.get-quote-button').on('click', function(event) {
         event.preventDefault();
-        console.log('Get quote button clicked');
         const requestForm = $('#requestForm');
 
         if (requestForm.length) {
             $('body').animate({
                 scrollTop: requestForm.offset().top
             }, 1000, function() {
-                console.log('Scroll animation completed');
                 requestForm.show();
             });
         } else {
@@ -18,10 +15,8 @@ $(document).ready(function() {
         }
     });
 
-    // Handle form submission via AJAX
-    $('#request-form').off('submit').on('submit', function(event) {
+    $('#request-form').on('submit', function(event) {
         event.preventDefault();
-        console.log('Request form submitted');
         var $form = $(this);
 
         $.ajax({
@@ -29,7 +24,6 @@ $(document).ready(function() {
             url: $form.attr('action'),
             data: $form.serialize(),
             success: function(response) {
-                console.log('AJAX request successful');
                 var responseHtml = $(response);
                 var newFormHtml = responseHtml.find('#request-form').html();
                 var successMessage = responseHtml.find('.flash-messages').html();
@@ -46,13 +40,11 @@ $(document).ready(function() {
                 }
             },
             error: function(response) {
-                console.error('AJAX request failed');
                 alert('An error occurred. Please try again.');
             }
         });
     });
 
-    // Scroll to the success message
     function scrollToSuccessMessage() {
         var successMessage = $('.flash-messages');
         if (successMessage.length > 0) {
@@ -64,7 +56,6 @@ $(document).ready(function() {
         }
     }
 
-    // Scroll back to the form in case of errors
     function scrollToForm() {
         var requestForm = $('#requestForm');
         if (requestForm.length > 0) {
